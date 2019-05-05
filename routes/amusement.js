311bba0b6e5df8073dcc242aceb99c0f95208bf9
zhengjun <webzheng@126.com>
2019-05-05 22:24:23 +0800

    修改

--- routes/amusement.js
+++ routes/amusement.js
@@ -50,8 +50,8 @@ router.get('/music/top500', (req, res) => {
         url: 'http://m.kugou.com/rank/info',
         method: 'post',
         data: {
-            'rankid': 8888, 
-            'page': 1, 
+            'rankid': 8888,
+            'page': 1,
             'json': true,
         },
         success: data => {
@@ -61,129 +61,129 @@ router.get('/music/top500', (req, res) => {
 });
 
 /* 音乐歌单 */
-router.get('/music/plist',(req,res)=>{
+router.get('/music/plist', (req, res) => {
     reqwest({
-        url:'http://m.kugou.com/plist/index',
-        method:'post',
-        data:{
-            'json':true
+        url: 'http://m.kugou.com/plist/index',
+        method: 'post',
+        data: {
+            'json': true
         },
-        success:data=>{
+        success: data => {
             res.json(data);
         }
     })
 });
 
 /* 歌单下的音乐列表 */
-router.get('/music/plist/list',(req,res)=>{
+router.get('/music/plist/list', (req, res) => {
     reqwest({
-        url:'http://m.kugou.com/plist/list',
-        method:'post',
-        data:{
+        url: 'http://m.kugou.com/plist/list',
+        method: 'post',
+        data: {
             'specialid': 125032,
-            'json':true,
+            'json': true,
         },
-        success:data=>{
+        success: data => {
             res.json(data);
         }
     })
 });
 
 /* 歌手分类 */
-router.get('/music/singer',(req,res)=>{
+router.get('/music/singer', (req, res) => {
     reqwest({
-        url:'http://m.kugou.com/singer/class',
-        method:'post',
-        data:{
-            'json':true,
+        url: 'http://m.kugou.com/singer/class',
+        method: 'post',
+        data: {
+            'json': true,
         },
-        success:data=>{
+        success: data => {
             res.json(data);
         }
     })
 });
 
 /* 歌手分类下面的歌手列表 */
-router.get('/music/singer/list',(req,res)=>{
+router.get('/music/singer/list', (req, res) => {
     reqwest({
-        url:'http://m.kugou.com/singer/list',
-        method:'post',
-        data:{
-            'json':true,
-            'classid':88
+        url: 'http://m.kugou.com/singer/list',
+        method: 'post',
+        data: {
+            'json': true,
+            'classid': 88
         },
-        success:data=>{
+        success: data => {
             res.json(data);
         }
     })
 });
 
 /* 歌手信息 */
-router.get('/music/singer/info',(req,res)=>{
+router.get('/music/singer/info', (req, res) => {
     reqwest({
-        url:'http://m.kugou.com/singer/info',
-        method:'post',
-        data:{
-            'singerid':3060,
-            'json':true,
+        url: 'http://m.kugou.com/singer/info',
+        method: 'post',
+        data: {
+            'singerid': 3060,
+            'json': true,
         },
-        success:data=>{
+        success: data => {
             res.json(data);
         }
     })
 });
 
 /* 歌曲音乐详情 */
-router.get('/music/songInfo',(req,res)=>{
+router.get('/music/songInfo', (req, res) => {
     reqwest({
-        url:'http://m.kugou.com/singer/info',
-        method:'post',
-        data:{
-            'hash':'CB7EE97F4CC11C4EA7A1FA4B516A5D97',
-            'cmd':'playInfo',
+        url: 'http://m.kugou.com/singer/info',
+        method: 'post',
+        data: {
+            'hash': 'CB7EE97F4CC11C4EA7A1FA4B516A5D97',
+            'cmd': 'playInfo',
         },
-        success:data=>{
+        success: data => {
             res.json(data);
         }
     })
 });
 
 /* 热门搜索列表 */
-router.get('/music/hotSearch',(req,res)=>{
+router.get('/music/hotSearch', (req, res) => {
     reqwest({
-        url:'http://mobilecdn.kugou.com/api/v3/search/hot',
-        method:'post',
-        data:{
-            'format':true,
-            'plat':0,
-            'count':30,
+        url: 'http://mobilecdn.kugou.com/api/v3/search/hot',
+        method: 'post',
+        data: {
+            'format': true,
+            'plat': 0,
+            'count': 30,
         },
-        success:data=>{
+        success: data => {
             res.json(data)
         }
     })
 });
 
 /* 音乐搜索 */
-router.get('/music/search',(req,res)=>{
+router.get('/music/search', (req, res) => {
     reqwest({
-        url:'http://mobilecdn.kugou.com/api/v3/search/song',
-        method:'post',
-        data:{
-            'format':true,
-            'keyword':'%E7%8E%8B%E5%8A%9B%E5%AE%8F',
-            'page':1,
-            'pagesize':30,
-            'showtype':1
+        url: 'http://mobilecdn.kugou.com/api/v3/search/song',
+        method: 'post',
+        data: {
+            'format': true,
+            'keyword': '%E7%8E%8B%E5%8A%9B%E5%AE%8F',
+            'page': 1,
+            'pagesize': 30,
+            'showtype': 1
         },
-        success:data=>{
+        success: data => {
             res.json(data)
         }
     })
 });
 
-router.get('/news', (req, res, next) => {
-    console.log(next);
+/* 头条新闻 */
+router.get('/news/header', (req, res, next) => {
     reqwest({
         url: 'http://c.3g.163.com/nc/article/list/T1467284926140/0-20.html',
         method: 'get',
@@ -191,7 +191,7 @@ router.get('/news', (req, res, next) => {
         success: data => {
             res.json(data);
         }
-    })
+    });
 });
 
 
