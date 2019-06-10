c9a00746037eb65271a0d80fd8d34caeb3be1139
zhnegjun924 <webzheng@126.com>
2019-06-10 20:17:19 +0800

    优化接口

--- routes/assets.js
+++ routes/assets.js
@@ -8,27 +8,28 @@ form.uploadDir = 'public/images/carousel';
 form.keepExtensions = true;
 form.multiples = true;
 
-router.get('/img/carouselIMg',(req,res)=>{
-    fs.readFile('public/data/carousel.json', 'utf8',(err,info)=>{
-        if(err) res.send(err);
+router.get('/img/carouselImg', (req, res) => {
+    fs.readFile('public/data/carousel.json', 'utf8', (err, info) => {
+        if (err) res.send(err);
         res.send(info)
     })
-})
+});
+
 router.post('/img/carousel', (req, res) => {
     form.parse(req, function (err, fields, files) {
-        fields.img = files.file.path.replace(/\\/g, "/");
-        fields.src = files.file.src;
-        
+        fields.name = files.file.name;
+        fields.url = 'zheng/'+files.file.path.replace(/\\/g, "/");
+
         fs.readFile('public/data/carousel.json', (err, data) => {
             if (err) res.send(errr);
             let carousel = JSON.parse(data.toString());
-            fields.id = (carousel.fileList.length-1)+1;
+            fields.id = (carousel.fileList.length - 1) + 1;
             carousel.fileList.push(fields);
-            
-            fs.writeFile('public/data/carousel.json', JSON.stringify(carousel,null,2),(err,info)=>{
+
+            fs.writeFile('public/data/carousel.json', JSON.stringify(carousel, null, 2), (err, info) => {
                 if (err) res.send(errr);
                 res.send('200')
-            })
+            });
         });
     });
 });
